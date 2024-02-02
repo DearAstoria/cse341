63af55bc27cfb8d7ffbcb508710dd8f05f083318
Jade <jadencamargo@gmail.com>
2024-02-02 14:46:50 -0800

    tad more

--- Project1/backend/routes/contacts.js
+++ Project1/backend/routes/contacts.js
@@ -5,10 +5,10 @@ const contactsController = require("../controllars/contacts");
 const router = express.Router();
 
 // GET /feed/posts
-//router.get("/", contactsController.getContacts);
+router.get("/", contactsController.getContacts);
 // GET 2nd request for specific id
 //_id = "65bbf1b4dd27051a5e1ef44f";
-router.get("/", contactsController.getSpecificContact);
+router.get("/singleContact", contactsController.getSpecificContact);
 
 // localhost:8080/contacts/
 module.exports = router;
