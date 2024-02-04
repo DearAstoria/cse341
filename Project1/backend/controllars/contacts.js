c2abce03ea4785a9fae080095aa85fb0d30f20cb
Jade <jadencamargo@gmail.com>
2024-02-04 02:16:16 -0800

    team assignment 3 completed before render

--- Project1/backend/controllars/contacts.js
+++ Project1/backend/controllars/contacts.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const mongodb = require("../db/connect");
 var ObjectId = require("mongodb").ObjectId;
 
@@ -7,6 +8,56 @@ const getContacts = async (req, res, next) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
+};
+// Create a POST route to create a new contact.
+// All fields are required.
+// Return the new contact id in the response body.
+//"firstName": "Manuel",
+//"lastName": "Camargo",
+//"email": "N/A",
+//"favoriteColor": "Azul",
+//"birthday": "06/17/49"
+const postContacts = async (request, response, next) => {
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("contacts")
+    .insertOne(request.body);
+  response.setHeader("Content-Type", "application/json");
+  response.status(200).json(result);
+  //response.redirect("/")
+};
+
+const putContact = async (request, response, next) => {
+  try {
+    id = request.params.id;
+    //console.log(id);
+    let o_id = new ObjectId(id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .updateOne({ _id: o_id }, { $set: request.body });
+    response.setHeader("Content-Type", "application/json");
+    response.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteContact = async (request, response, next) => {
+  try {
+    //o_id = new ObjectId(request.params.id); too annoying since the ids will change when creating and deleting the same contact.
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("contacts")
+      .deleteOne(request.body);
+    response.setHeader("Content-Type", "application/json");
+    response.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // From:https://stackoverflow.com/questions/8233014/how-do-i-search-for-an-object-by-its-objectid-in-the-mongo-console
@@ -34,4 +85,10 @@ const getSpecificContact = async (req, res, next) => {
   });
 };
 
-module.exports = { getContacts, getSpecificContact };
+module.exports = {
+  getContacts,
+  getSpecificContact,
+  postContacts,
+  putContact,
+  deleteContact,
+};
--- Project1/backend/routes/contacts.js
+++ Project1/backend/routes/contacts.js
@@ -9,6 +9,17 @@ router.get("/", contactsController.getContacts);
 // GET 2nd request for specific id
 //_id = "65bbf1b4dd27051a5e1ef44f";
 router.get("/singleContact", contactsController.getSpecificContact);
+// Create a POST route to create a new contact.
+// All fields are required.
+// Return the new contact id in the response body.
+router.post("/", contactsController.postContacts);
+//Create a PUT route to update a contact. This route should allow for a url similar to this: api-url-path/contacts/id-to-modify. (The id won't be modified, it will just be the means of finding a specific document in the database.) Return an http status code representing the successful completion of the request.
+// If a user makes a put request to this address with and :id attached
+// Then we will call putContact which will update on contact
+router.put("/:id", contactsController.putContact);
+// Create a DELETE route to delete a contact.
+// Return an http status code representing the successful completion of the request.
+router.delete("/", contactsController.deleteContact);
 
 // localhost:8080/contacts/
 module.exports = router;
